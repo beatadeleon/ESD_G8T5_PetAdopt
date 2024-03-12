16f2d13998cfd5d47895f907cef420cc3cea28fa
beatadeleon <beataleon.2022@smu.edu.sg>
2024-03-13 00:38:01 +0800

    changes to notifications

--- notifications/frontend.js
+++ notifications/frontend.js
@@ -7,6 +7,7 @@ const app = Vue.createApp({
     methods: {
         notify(arg) {
             url = `http://localhost:5100/${arg}`
+            // Different json data passed for each notification type
             axios.post(url, {"email": this.email}, { withCredentials: true })
             .then(response =>{
                 console.log(response.data)
