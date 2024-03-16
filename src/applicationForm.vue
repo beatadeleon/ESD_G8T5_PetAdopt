102ae40c3a5dbde42de3ecf2e74087d41388042c
beatadeleon <beataleon.2022@smu.edu.sg>
2024-03-16 22:54:53 +0800

    changed from adoption > manageadopt complex microservice
    UI makes POST request to manage adopt request microservice first

--- src/applicationForm.vue
+++ src/applicationForm.vue
@@ -81,7 +81,7 @@ const submitApplication = async () => {
     };
     
     // Make an HTTP POST request to the Flask server
-    const response = await fetch('http://localhost:5110/submit_application', {
+    const response = await fetch('http://localhost:5300/create_application', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
