e353a6804a92d8c42103d0e42734ef21d7a12c92
beatadeleon <beataleon.2022@smu.edu.sg>
2024-03-27 16:32:45 +0800

    cancel

--- CancelRequest/cancelRequest.py
+++ CancelRequest/cancelRequest.py
@@ -22,8 +22,6 @@ def cancel_request():
         try:
 
             request_data = request.get_json().get("request")
-=======
-            request_data = request.get_json()
             print(request_data)
 
             print("\nReceived a request in JSON:", request_data)
