2541ebb24d64ebddc025492d139a9c573ae3c34a
cakemerc01 <148352428+cakemerc01@users.noreply.github.com>
2024-03-28 11:46:55 +0800

    swagger

--- booking/booking.py
+++ booking/booking.py
@@ -6,10 +6,19 @@ from firebase_admin import credentials
 from firebase_admin import db
 import requests
 
-
+from flasgger import Swagger
 
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
+
+# Initialize flasgger 
+app.config['SWAGGER'] = {
+    'title': 'Book microservice API',
+    'version': 1.0,
+    "openapi": "3.0.2",
+    'description': 'Allows create, retrieve, update, and delete of books'
+}
+swagger = Swagger(app)
 
 cred = credentials.Certificate('../petadopt-e0fe8-firebase-adminsdk-l81sh-f8914d3037.json')
 firebase_admin.initialize_app(cred, {
@@ -23,6 +32,32 @@ CALENDLY_BASE_URL = 'https://api.calendly.com'
 
 @app.route("/update_calendly_uuid", methods=['POST'])
 def update_calendly_uuid():
+    """
+    Update Calendly UUID for a user
+    ---
+    requestBody:
+        description: User's data
+        required: true
+        content:
+            application/json:
+                schema:
+                    properties:
+                        email: 
+                            type: string
+                            description: User's email
+                        calendlyUuid: 
+                            type: string
+                            description: User's Calendly UUID
+    responses:
+        200:
+            description: Calendly UUID updated successfully
+        400:
+            description: Missing email or calendlyUuid in the request
+        404:
+            description: User not found or No users with the provided email
+        500:
+            description: An error occurred during the operation
+    """
     if request.is_json:
         try:
             data = request.get_json()
@@ -72,6 +107,30 @@ def update_calendly_uuid():
 
 @app.route("/process_cancellation", methods=['POST'])
 def process_cancellation():
+    """
+    Process cancellation of Calendly booking for a user
+    ---
+    requestBody:
+        description: User's data
+        required: true
+        content:
+            application/json:
+                schema:
+                    properties:
+                        email: 
+                            type: string
+                            description: User's email
+                        calendlyUuid: 
+                            type: string
+                            description: User's Calendly UUID
+    responses:
+        200:
+            description: Calendly booking cancelled successfully
+        400:
+            description: Invalid JSON input or email is required
+        500:
+            description: Internal server error during the cancellation process
+    """
     if request.is_json:
         try:
             request_data = request.get_json()
